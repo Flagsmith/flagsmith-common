f8dffffd68039517e8c2fbe1721fcca2c00f4597
Matthew Elwell <matthew.elwell@flagsmith.com>
2024-07-16 16:02:28 +0100

    Add serializers for feature versioning change requests

---
+++ common/features/serializers.py
@@ -0,0 +1,68 @@
+from drf_writable_nested.serializers import WritableNestedModelSerializer
+from rest_framework import serializers
+
+from django.apps import apps
+
+
+class FeatureStateValueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = apps.get_model("features", "FeatureStateValue")
+        fields = ("type", "string_value", "integer_value", "boolean_value")
+
+
+class CreateSegmentOverrideFeatureSegmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = apps.get_model("features", "FeatureSegment")
+        fields = ("id", "segment", "priority", "uuid")
+
+
+class MultivariateFeatureStateValueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = apps.get_model("features.multivariate", "MultivariateFeatureStateValue")
+        fields = (
+            "id",
+            "multivariate_feature_option",
+            "percentage_allocation",
+        )
+
+
+class CreateSegmentOverrideFeatureStateSerializer(WritableNestedModelSerializer):
+    feature_state_value = FeatureStateValueSerializer()
+    feature_segment = CreateSegmentOverrideFeatureSegmentSerializer(
+        required=False, allow_null=True
+    )
+    multivariate_feature_state_values = MultivariateFeatureStateValueSerializer(
+        many=True, required=False
+    )
+
+    class Meta:
+        model = apps.get_model("features", "FeatureState")
+        fields = (
+            "id",
+            "feature",
+            "enabled",
+            "feature_state_value",
+            "feature_segment",
+            "deleted_at",
+            "uuid",
+            "created_at",
+            "updated_at",
+            "live_from",
+            "environment",
+            "identity",
+            "change_request",
+            "multivariate_feature_state_values",
+        )
+
+        read_only_fields = (
+            "id",
+            "deleted_at",
+            "uuid",
+            "created_at",
+            "updated_at",
+            "live_from",
+            "environment",
+            "identity",
+            "change_request",
+            "feature",
+        )
---
+++ common/features/versioning/serializers.py
@@ -0,0 +1,11 @@
+from common.features.serializers import CreateSegmentOverrideFeatureStateSerializer
+
+
+class EnvironmentFeatureVersionFeatureStateSerializer(
+    CreateSegmentOverrideFeatureStateSerializer
+):
+    class Meta(CreateSegmentOverrideFeatureStateSerializer.Meta):
+        read_only_fields = (
+            CreateSegmentOverrideFeatureStateSerializer.Meta.read_only_fields
+            + ("feature",)
+        )
