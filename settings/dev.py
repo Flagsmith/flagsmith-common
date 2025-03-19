28ec349564b3a2e2b239a1cc96c5701acb6f869b
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-03-19 11:35:30 +0000

    fix types, document

--- settings/dev.py
+++ settings/dev.py
@@ -1,7 +1,9 @@
+# Settings expected by `mypy_django_plugin`
 SENDGRID_API_KEY: str
 AWS_SES_REGION_ENDPOINT: str
 SEGMENT_RULES_CONDITIONS_LIMIT: int
 
+# Settings required for tests
 INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
