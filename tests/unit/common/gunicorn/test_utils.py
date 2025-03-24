ab4b7ed5eaaff80034887b30c3da4a0ec4cc89be
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-03-24 19:19:56 +0000

    fix test

--- tests/unit/common/gunicorn/test_utils.py
+++ tests/unit/common/gunicorn/test_utils.py
@@ -36,6 +36,7 @@ def test_django_wsgi_application__defaults__expected_config(
 
 
 def test_run_server__default_config_file__runs_expected(
+    unused_tcp_port: int,
     mocker: pytest_mock.MockerFixture,
 ) -> None:
     # Given
@@ -43,7 +44,9 @@ def test_run_server__default_config_file__runs_expected(
     mocker.patch("os.fork").return_value = 0
     mark_process_dead_mock = mocker.patch("common.gunicorn.conf.mark_process_dead")
 
-    def delay_kill(pid: int = os.getpid()) -> None:
+    pid = os.getpid()
+
+    def delay_kill(pid: int = pid) -> None:
         time.sleep(0.1)
         os.kill(pid, signal.SIGTERM)
 
@@ -51,7 +54,7 @@ def test_run_server__default_config_file__runs_expected(
 
     # When
     with pytest.raises(SystemExit):
-        run_server()
+        run_server({"bind": f"0.0.0.0:{unused_tcp_port}"})
 
     # Then
-    mark_process_dead_mock.assert_called()
+    mark_process_dead_mock.assert_called_once_with(pid)
