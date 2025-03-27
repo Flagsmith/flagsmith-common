b674094e44108423b7acd033ad8735a5ed4c4b91
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-03-27 14:36:07 +0000

    correct assertion

--- tests/unit/task_processor/test_unit_task_processor_processor.py
+++ tests/unit/task_processor/test_unit_task_processor_processor.py
@@ -600,7 +600,7 @@ def test_run_tasks__expected_metrics(
         value=mocker.ANY,
         labels={
             "task_identifier": raise_exception_task_identifier,
-            "result": "success",
+            "result": "failure",
         },
     )
 
