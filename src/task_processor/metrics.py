8ced849c0120c80019c1d1eb3cc65dda026374af
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-03-27 14:20:10 +0000

    prevent exposing task processor netrics from api

--- src/task_processor/metrics.py
+++ src/task_processor/metrics.py
@@ -1,19 +1,22 @@
 import prometheus_client
+from django.conf import settings
 
 from common.prometheus import Histogram
 
-task_processor_task_duration_seconds = Histogram(
-    "task_processor_task_duration_seconds",
-    "Task processor task duration in seconds",
+task_processor_enqueued_tasks_total = prometheus_client.Counter(
+    "task_processor_enqueued_tasks_total",
+    "Total number of enqueued tasks",
     ["task_identifier"],
 )
-task_processor_queued_tasks_total = prometheus_client.Counter(
-    "task_processor_queued_tasks_total",
-    "Total number of queued tasks",
-    ["task_identifier"],
-)
-task_processor_finished_tasks_total = prometheus_client.Counter(
-    "task_processor_finished_tasks_total",
-    "Total number of finished tasks",
-    ["task_identifier", "status"],
-)
+
+if settings.TASK_PROCESSOR_MODE:
+    task_processor_finished_tasks_total = prometheus_client.Counter(
+        "task_processor_finished_tasks_total",
+        "Total number of finished tasks",
+        ["task_identifier", "result"],
+    )
+    task_processor_task_duration_seconds = Histogram(
+        "task_processor_task_duration_seconds",
+        "Task processor task duration in seconds",
+        ["task_identifier", "result"],
+    )
