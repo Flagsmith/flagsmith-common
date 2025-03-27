9e224f121ffbb32d332f648d31b9089cacfce6c7
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-03-27 17:37:25 +0000

    feat: Add Task processor (#26)
    * feat: Add Task processor
    - Port `flagsmith/flagsmith-task-processor`
    - Port `waitfordb` management command
    - Introduce PostgreSQL for tests
    - Bump Poetry from 2.0.1 to 2.1.1
    
    ---------
    
    Co-authored-by: Matthew Elwell <matthew.elwell@flagsmith.com>

--- settings/dev.py
+++ settings/dev.py
@@ -1,4 +1,12 @@
+from datetime import time, timedelta
+
+import dj_database_url
 import prometheus_client
+from environs import Env
+
+from task_processor.task_run_method import TaskRunMethod
+
+env = Env()
 
 # Settings expected by `mypy_django_plugin`
 AWS_SES_REGION_ENDPOINT: str
@@ -8,15 +16,18 @@ PROMETHEUS_ENABLED = True
 
 # Settings required for tests
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "common.sqlite3",
-    }
+    "default": dj_database_url.parse(
+        env(
+            "DATABASE_URL",
+            default="postgresql://postgres:password@localhost:5432/flagsmith",
+        )
+    )
 }
 INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "common.core",
+    "task_processor",
 ]
 MIDDLEWARE = [
     "common.gunicorn.middleware.RouteLoggerMiddleware",
@@ -26,3 +37,16 @@ PROMETHEUS_HISTOGRAM_BUCKETS = prometheus_client.Histogram.DEFAULT_BUCKETS
 ROOT_URLCONF = "common.core.urls"
 TIME_ZONE = "UTC"
 USE_TZ = True
+
+ENABLE_CLEAN_UP_OLD_TASKS = True
+ENABLE_TASK_PROCESSOR_HEALTH_CHECK = True
+RECURRING_TASK_RUN_RETENTION_DAYS = 15
+TASK_DELETE_BATCH_SIZE = 2000
+TASK_DELETE_INCLUDE_FAILED_TASKS = False
+TASK_DELETE_RETENTION_DAYS = 15
+TASK_DELETE_RUN_EVERY = timedelta(days=1)
+TASK_DELETE_RUN_TIME = time(5, 0, 0)
+TASK_RUN_METHOD = TaskRunMethod.TASK_PROCESSOR
+
+# Avoid models.W042 warnings
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
---
+++ tests/unit/task_processor/test_unit_task_processor_decorators.py
@@ -0,0 +1,229 @@
+import json
+import typing
+from datetime import timedelta
+from unittest.mock import MagicMock
+
+import pytest
+from pytest_django import DjangoCaptureOnCommitCallbacks
+from pytest_django.fixtures import SettingsWrapper
+from pytest_mock import MockerFixture
+
+from task_processor.decorators import (
+    register_recurring_task,
+    register_task_handler,
+)
+from task_processor.exceptions import InvalidArgumentsError
+from task_processor.models import RecurringTask, Task, TaskPriority
+from task_processor.task_registry import get_task, initialise
+from task_processor.task_run_method import TaskRunMethod
+
+if typing.TYPE_CHECKING:
+    # This import breaks private-package-test workflow in core
+    from tests.unit.task_processor.conftest import GetTaskProcessorCaplog
+
+
+@pytest.fixture
+def mock_thread_class(
+    mocker: MockerFixture,
+) -> MagicMock:
+    mock_thread_class = mocker.patch(
+        "task_processor.decorators.Thread",
+        return_value=mocker.MagicMock(),
+    )
+    return mock_thread_class
+
+
+@pytest.mark.django_db
+def test_register_task_handler_run_in_thread__transaction_commit__true__default(
+    get_task_processor_caplog: "GetTaskProcessorCaplog",
+    mock_thread_class: MagicMock,
+    django_capture_on_commit_callbacks: DjangoCaptureOnCommitCallbacks,
+) -> None:
+    # Given
+    caplog = get_task_processor_caplog()
+
+    @register_task_handler()
+    def my_function(*args: str, **kwargs: str) -> None:
+        pass
+
+    mock_thread = mock_thread_class.return_value
+
+    args = ("foo",)
+    kwargs = {"bar": "baz"}
+
+    # When
+    with django_capture_on_commit_callbacks(execute=True):
+        my_function.run_in_thread(args=args, kwargs=kwargs)
+
+    # Then
+    mock_thread_class.assert_called_once_with(
+        target=my_function.unwrapped, args=args, kwargs=kwargs, daemon=True
+    )
+    mock_thread.start.assert_called_once()
+
+    assert len(caplog.records) == 1
+    assert (
+        caplog.records[0].message == "Running function my_function in unmanaged thread."
+    )
+
+
+def test_register_task_handler_run_in_thread__transaction_commit__false(
+    get_task_processor_caplog: "GetTaskProcessorCaplog",
+    mock_thread_class: MagicMock,
+) -> None:
+    # Given
+    caplog = get_task_processor_caplog()
+
+    @register_task_handler(transaction_on_commit=False)
+    def my_function(*args: typing.Any, **kwargs: typing.Any) -> None:
+        pass
+
+    mock_thread = mock_thread_class.return_value
+
+    args = ("foo",)
+    kwargs = {"bar": "baz"}
+
+    # When
+    my_function.run_in_thread(args=args, kwargs=kwargs)
+
+    # Then
+    mock_thread_class.assert_called_once_with(
+        target=my_function.unwrapped, args=args, kwargs=kwargs, daemon=True
+    )
+    mock_thread.start.assert_called_once()
+
+    assert len(caplog.records) == 1
+    assert (
+        caplog.records[0].message == "Running function my_function in unmanaged thread."
+    )
+
+
+def test_register_recurring_task(
+    mocker: MockerFixture,
+    db: None,
+    run_by_processor: None,
+) -> None:
+    # Given
+    mock = mocker.Mock()
+    mock.__name__ = "a_function"
+    task_kwargs = {"first_arg": "foo", "second_arg": "bar"}
+    run_every = timedelta(minutes=10)
+    task_identifier = "mock.a_function"
+
+    # When
+    register_recurring_task(
+        run_every=run_every,
+        kwargs=task_kwargs,
+    )(mock)
+
+    # Then
+    initialise()
+
+    task = RecurringTask.objects.get(task_identifier=task_identifier)
+    assert task.serialized_kwargs == json.dumps(task_kwargs)
+    assert task.run_every == run_every
+
+    assert get_task(task_identifier)
+    assert task.callable is mock
+
+
+def test_register_recurring_task_does_nothing_if_not_run_by_processor(
+    mocker: MockerFixture,
+    db: None,
+) -> None:
+    # Given
+
+    task_kwargs = {"first_arg": "foo", "second_arg": "bar"}
+    run_every = timedelta(minutes=10)
+    task_identifier = "test_unit_task_processor_decorators.some_function"
+
+    # When
+    @register_recurring_task(
+        run_every=run_every,
+        kwargs=task_kwargs,
+    )
+    def some_function(first_arg: str, second_arg: str) -> None:
+        pass
+
+    # Then
+    assert not RecurringTask.objects.filter(task_identifier=task_identifier).exists()
+    with pytest.raises(KeyError):
+        assert get_task(task_identifier)
+
+
+def test_register_task_handler_validates_inputs() -> None:
+    # Given
+    @register_task_handler()
+    def my_function(*args: typing.Any, **kwargs: typing.Any) -> None:
+        pass
+
+    class NonSerializableObj:
+        pass
+
+    # When
+    with pytest.raises(InvalidArgumentsError):
+        my_function(NonSerializableObj())
+
+
+@pytest.mark.parametrize(
+    "task_run_method", (TaskRunMethod.SEPARATE_THREAD, TaskRunMethod.SYNCHRONOUSLY)
+)
+def test_inputs_are_validated_when_run_without_task_processor(
+    settings: SettingsWrapper, task_run_method: TaskRunMethod
+) -> None:
+    # Given
+    settings.TASK_RUN_METHOD = task_run_method
+
+    @register_task_handler()
+    def my_function(*args: typing.Any, **kwargs: typing.Any) -> None:
+        pass
+
+    class NonSerializableObj:
+        pass
+
+    # When
+    with pytest.raises(InvalidArgumentsError):
+        my_function.delay(args=(NonSerializableObj(),))
+
+
+def test_delay_returns_none_if_task_queue_is_full(
+    settings: SettingsWrapper, db: None
+) -> None:
+    # Given
+    settings.TASK_RUN_METHOD = TaskRunMethod.TASK_PROCESSOR
+
+    @register_task_handler(queue_size=1)
+    def my_function(*args: typing.Any, **kwargs: typing.Any) -> None:
+        pass
+
+    for _ in range(10):
+        Task.objects.create(
+            task_identifier="test_unit_task_processor_decorators.my_function"
+        )
+
+    # When
+    task = my_function.delay()
+
+    # Then
+    assert task is None
+
+
+def test_can_create_task_with_priority(settings: SettingsWrapper, db: None) -> None:
+    # Given
+    settings.TASK_RUN_METHOD = TaskRunMethod.TASK_PROCESSOR
+
+    @register_task_handler(priority=TaskPriority.HIGH)
+    def my_function(*args: typing.Any, **kwargs: typing.Any) -> None:
+        pass
+
+    for _ in range(10):
+        Task.objects.create(
+            task_identifier="test_unit_task_processor_decorators.my_function"
+        )
+
+    # When
+    task = my_function.delay()
+
+    # Then
+    assert task
+    assert task.priority == TaskPriority.HIGH
