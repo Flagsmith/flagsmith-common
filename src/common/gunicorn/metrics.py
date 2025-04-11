13deb7ad8e8692ec1590bb9b21cd365d9a3a51a3
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-04-11 17:41:10 +0100

    add default buckets

--- src/common/gunicorn/metrics.py
+++ src/common/gunicorn/metrics.py
@@ -1,6 +1,7 @@
 import prometheus_client
 from django.conf import settings
 
+from common.gunicorn.constants import HTTP_SERVER_RESPONSE_SIZE_DEFAULT_BUCKETS
 from common.prometheus import Histogram
 
 flagsmith_http_server_requests_total = prometheus_client.Counter(
@@ -17,5 +18,9 @@ flagsmith_http_server_response_size_bytes = Histogram(
     "flagsmith_http_server_response_size_bytes",
     "HTTP response size in bytes",
     ["route", "method"],
-    buckets=settings.PROMETHEUS_HTTP_SERVER_RESPONSE_SIZE_HISTOGRAM_BUCKETS,
+    buckets=getattr(
+        settings,
+        "PROMETHEUS_HTTP_SERVER_RESPONSE_SIZE_HISTOGRAM_BUCKETS",
+        HTTP_SERVER_RESPONSE_SIZE_DEFAULT_BUCKETS,
+    ),
 )
