8639d042393cfafa3cff5a2d13a191505d6b9fc9
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-04-16 12:18:31 +0100

    fix tests

--- src/common/test_tools/plugin.py
+++ src/common/test_tools/plugin.py
@@ -36,25 +36,33 @@ assert_metric = pytest.fixture(assert_metric_impl)
 
 
 @pytest.fixture()
-def saas_mode(fs: FakeFilesystem) -> None:
+def saas_mode(fs: FakeFilesystem) -> Generator[None, None, None]:
     from common.core.utils import is_saas
 
     is_saas.cache_clear()
-
     fs.create_file("./SAAS_DEPLOYMENT")
+
+    yield
+
+    is_saas.cache_clear()
 
 
 @pytest.fixture()
-def enterprise_mode(fs: FakeFilesystem) -> None:
+def enterprise_mode(fs: FakeFilesystem) -> Generator[None, None, None]:
     from common.core.utils import is_enterprise
 
     is_enterprise.cache_clear()
-
     fs.create_file("./ENTERPRISE_VERSION")
+
+    yield
+
+    is_enterprise.cache_clear()
 
 
 @pytest.fixture(autouse=True)
-def flagsmith_markers_marked(request: pytest.FixtureRequest) -> None:
+def flagsmith_markers_marked(
+    request: pytest.FixtureRequest,
+) -> None:
     for marker in request.node.iter_markers():
         if marker.name == "saas_mode":
             request.getfixturevalue("saas_mode")
