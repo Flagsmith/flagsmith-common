16fdcc3d77e032c589c8e0ea15b217225396a733
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-04-25 22:33:29 +0100

    improve docs

--- src/common/core/metrics.py
+++ src/common/core/metrics.py
@@ -4,7 +4,7 @@ from common.core.utils import get_version_info
 
 flagsmith_build_info = prometheus_client.Gauge(
     "flagsmith_build_info",
-    "Flagsmith version and build information",
+    "Flagsmith version and build information.",
     ["ci_commit_sha", "version"],
     multiprocess_mode="livemax",
 )
