786e4904d885b1af7e04856bd141673e5dc77805
Matthew Elwell <matthew.elwell@flagsmith.com>
2025-11-10 18:31:55 +0000

    Move to gunicorn conf.py

--- src/common/core/utils.py
+++ src/common/core/utils.py
@@ -1,12 +1,9 @@
 import json
 import logging
-import os
 import pathlib
 import random
-import shutil
 from functools import lru_cache
 from itertools import cycle
-from tempfile import gettempdir
 from typing import (
     TYPE_CHECKING,
     Iterator,
@@ -23,7 +20,6 @@ from django.db import connections
 from django.db.utils import OperationalError
 
 from common.core import ReplicaReadStrategy
-from common.core.constants import DEFAULT_PROMETHEUS_MULTIPROC_DIR_NAME
 
 if TYPE_CHECKING:
     from django.contrib.auth.models import AbstractBaseUser
@@ -202,30 +198,3 @@ def using_database_replica(
         return manager
 
     return manager.db_manager(chosen_replica)
-
-
-def prepare_prom_multiproc_dir() -> None:
-    prom_dir = pathlib.Path(
-        os.environ.setdefault(
-            "PROMETHEUS_MULTIPROC_DIR",
-            os.path.join(gettempdir(), DEFAULT_PROMETHEUS_MULTIPROC_DIR_NAME),
-        )
-    )
-
-    if prom_dir.exists():
-        for p in prom_dir.rglob("*"):
-            try:
-                # Ensure that the cleanup doesn't silently fail on
-                # files and subdirs created by other users.
-                p.chmod(0o777)
-            except Exception:
-                pass
-
-        shutil.rmtree(prom_dir, ignore_errors=True)
-
-    prom_dir.mkdir(parents=True, exist_ok=True)
-
-    # While `mkdir` sets mode=0o777 by default, this can be affected by umask resulting in
-    # lesser permissions for other users. This step ensures the directory is writable for
-    # all users.
-    prom_dir.chmod(0o777)
--- src/common/gunicorn/conf.py
+++ src/common/gunicorn/conf.py
@@ -4,9 +4,15 @@ This module is used as a default configuration file for Gunicorn.
 It is used to correctly support Prometheus metrics in a multi-process environment.
 """
 
+import os
+import pathlib
+import shutil
 import typing
+from tempfile import gettempdir
 
 from prometheus_client.multiprocess import mark_process_dead
+
+from common.core.constants import DEFAULT_PROMETHEUS_MULTIPROC_DIR_NAME
 
 if typing.TYPE_CHECKING:  # pragma: no cover
     from gunicorn.arbiter import Arbiter  # type: ignore[import-untyped]
@@ -16,3 +22,31 @@ if typing.TYPE_CHECKING:  # pragma: no cover
 def worker_exit(server: "Arbiter", worker: "Worker") -> None:
     """Detach the process Prometheus metrics collector when a worker exits."""
     mark_process_dead(worker.pid)  # type: ignore[no-untyped-call]
+
+
+def on_starting(server: "Arbiter") -> None:
+    """Prepare multiprocessing directory for prometheus metrics collector."""
+    prom_dir = pathlib.Path(
+        os.environ.setdefault(
+            "PROMETHEUS_MULTIPROC_DIR",
+            os.path.join(gettempdir(), DEFAULT_PROMETHEUS_MULTIPROC_DIR_NAME),
+        )
+    )
+
+    if prom_dir.exists():
+        for p in prom_dir.rglob("*"):
+            try:
+                # Ensure that the cleanup doesn't silently fail on
+                # files and subdirs created by other users.
+                p.chmod(0o777)
+            except Exception:
+                pass
+
+        shutil.rmtree(prom_dir, ignore_errors=True)
+
+    prom_dir.mkdir(parents=True, exist_ok=True)
+
+    # While `mkdir` sets mode=0o777 by default, this can be affected by umask resulting in
+    # lesser permissions for other users. This step ensures the directory is writable for
+    # all users.
+    prom_dir.chmod(0o777)
