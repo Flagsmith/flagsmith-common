1aae629de018cd9c4b21d0b51399748c931c5f1d
Matthew Elwell <matthew.elwell@flagsmith.com>
2025-11-10 19:21:23 +0000

    Improve docstring

--- src/common/prometheus/__init__.py
+++ src/common/prometheus/__init__.py
@@ -5,8 +5,9 @@ _utils = ("Histogram",)
 
 def __getattr__(name: str) -> Any:
     """
-    Since utils imports settings, we lazy load any objects that we want to import to
-    prevent Django's settings-at-import-time trap
+    Since utils imports django.conf.settings, we lazy load any objects that
+    we want to import to prevent django.core.exceptions.ImproperlyConfigured
+    due to settings not being configured.
     """
     if name in _utils:
         from common.prometheus import utils
