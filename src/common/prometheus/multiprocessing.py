6e741bca976e56e569c942f6eecf49e418971cd7
Matthew Elwell <matthew.elwell@flagsmith.com>
2025-11-10 20:11:00 +0000

    Add try/except

--- src/common/prometheus/multiprocessing.py
+++ src/common/prometheus/multiprocessing.py
@@ -30,4 +30,7 @@ def prepare_prom_multiproc_dir() -> None:
     # While `mkdir` sets mode=0o777 by default, this can be affected by umask resulting in
     # lesser permissions for other users. This step ensures the directory is writable for
     # all users.
-    prom_dir.chmod(0o777)
+    try:
+        prom_dir.chmod(0o777)
+    except PermissionError:
+        pass
