0319fd042b85655c730451c187caeea146b3d938
Kim Gustyr <kim.gustyr@flagsmith.com>
2025-11-12 14:34:08 +0000

    fix: Remove `PROMETHEUS_MULTIPROC_DIR` cleanup logic (#123)

--- src/common/core/utils.py
+++ src/common/core/utils.py
@@ -1,9 +1,7 @@
 import json
 import logging
-import os
 import pathlib
 import random
-import shutil
 from functools import lru_cache
 from itertools import cycle
 from typing import (
@@ -200,30 +198,3 @@ def using_database_replica(
         return manager
 
     return manager.db_manager(chosen_replica)
-
-
-def clear_directory(directory_path: str) -> None:
-    """
-    Safely clear a directory including all subdirectories and files.
-    """
-    for p in pathlib.Path(directory_path).rglob("*"):
-        try:
-            # Ensure that the cleanup doesn't silently fail on
-            # files and subdirs created by other users.
-            p.chmod(0o777)
-        except (PermissionError, FileNotFoundError):  # pragma: no cover
-            pass
-
-    shutil.rmtree(directory_path, ignore_errors=True)
-
-
-def make_writable_directory(directory_path: str) -> None:
-    os.makedirs(directory_path, exist_ok=True)
-
-    try:
-        # While `mkdir` sets mode=0o777 by default, this can be affected by umask
-        # resulting in lesser permissions for other users. This step ensures the
-        # directory is writable for all users.
-        os.chmod(directory_path, 0o777)
-    except PermissionError:  # pragma: no cover
-        pass
